e1818a18b6af67315631f982b5a9a0f59642ccc4
Mikko Pajula <mtpajula@gmail.com>
2018-02-04 18:03:18 +0200

    serial back and pin 0 for relay

--- arduino/IOT0101/IOT0101.ino
+++ arduino/IOT0101/IOT0101.ino
@@ -10,12 +10,10 @@ const char* password = "...";
 // Device name
 const String devname = "esprelay01";
 // digital output pin number
-const int dev = 2;
+const int dev = 0;
 // ===
 
 ESP8266WebServer server(80);
-
-
 String devStatus = "off";
 
 String createJsonKeyValue(String key, String value) {
@@ -59,11 +57,23 @@ void setup(void){
   pinMode(dev, OUTPUT);
   digitalWrite(dev, 0);
   
+  Serial.begin(115200);
   WiFi.begin(ssid, password);
+  Serial.println("");
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
   }
 
   server.on("/", handleRoot);
@@ -77,6 +87,7 @@ void setup(void){
   server.onNotFound(handleNotFound);
 
   server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop(void){
